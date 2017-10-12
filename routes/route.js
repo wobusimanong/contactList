c1aefd1e265a5c6a079ae2acad1c7a2dca504eab
wobusimanong <269410834@qq.com>
2017-10-11 20:08:51 -0400

    a mean stack project

---
+++ routes/route.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const router = express.Router();
+
+const Contact = require('../models/contacts');
+
+//retrieving contacts
+router.get('/contacts', (req, res, next)=>{
+    Contact.find(function(err, contacts) {
+        res.json(contacts);
+    });
+});
+
+//add contact
+router.post('/contact', (req, res, next)=>{
+    //logic to add contact
+    let newContact = new Contact({
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        phone: req.body.phone
+    });
+    newContact.save((err,contact)=> {
+        if(err)
+        {
+            res.json({msg:'Failed to add contact'});
+        }
+        else {
+            res.json({msg: 'Contact add successfully'});
+        }
+    });
+});
+
+//delete contacts
+router.delete('/contact/:id', (req, res, next)=>{
+    //logic to delete contact
+    Contact.remove({_id: req.params.id}, function(err, result) {
+        if(err)
+        {
+            res.json(err);
+        }
+        else {
+            res.json(result);
+        }
+    });
+});
+
+module.exports = router;
